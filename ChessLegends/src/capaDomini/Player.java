4c10053bfa1e0cbb1561745d0a05f70a3b5a9c51
Solde <david.soldevila.puigbi@est.fib.upc.edu>
2019-03-21 10:29:53 +0100

    update

---
+++ ChessLegends/src/capaDomini/Player.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capaDomini;
+
+/**
+ *
+ * @author Daniel Palomo
+ */
+public abstract class Player {
+    //private 
+
+    private static final int DEFAULT_ELO = 1000;
+
+    protected String id;
+    protected int wins;
+    protected int loses;
+    protected float ELO;
+    protected float OP_rating;
+
+
+    //public methods
+    public abstract void play_turn();
+    //public constructors & get/set
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLoses() {
+        return loses;
+    }
+
+    public void setLoses(int loses) {
+        this.loses = loses;
+    }
+
+    public void incLoses() {
+        this.loses = this.loses+1;
+    }
+
+    public void incWinds() {
+        this.wins = this.wins+1;
+    }
+
+    public float getELO() {
+        return ELO;
+    }
+
+    public void setELO(float ELO) {
+        this.ELO = ELO;
+    }
+    public float getOP_rating() {
+        return OP_rating;
+    }
+
+    public void setOP_rating(float OP_rating) {
+        this.OP_rating = OP_rating;
+    }
+
+    public void incOP_rating(float OP_rating) {
+        this.OP_rating = this.OP_rating + OP_rating;
+    }
+}
---
+++ ChessLegends/src/chesslegends/ChessLegends.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chesslegends;
+
+/**
+ *
+ * @author David Soldevila
+ */
+public class ChessLegends {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
