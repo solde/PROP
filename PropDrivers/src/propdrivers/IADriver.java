29d0f61e714df0927f4493615e5a741fe27004ef
RadioConica <santosfer7@gmail.com>
2019-04-20 18:42:52 +0200

    IAalphaver

---
+++ PropDrivers/src/propdrivers/IADriver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package propdrivers;
+
+/**
+ *
+ * @author Arnau Santos
+ */
+public class IADriver {
+    //De moment només teniu aquest bonic missatge UwU
+}
