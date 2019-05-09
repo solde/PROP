6b1e73b75e06027653d9b903764a682fcd848372
Solde <david.soldevila.puigbi@est.fib.upc.edu>
2019-05-09 11:51:40 +0200

    update

--- ChessLegends/src/capaDades/CtrlDades.java
+++ ChessLegends/src/capaDades/CtrlDades.java
@@ -6,18 +6,8 @@
 package capaDades;
 
 import Exception.chessException;
-import capaDomini.Human;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import static java.lang.System.in;
 
 /**
  *
@@ -27,9 +17,9 @@ import static java.lang.System.in;
 //--THIS WHOLE CLASS IS WORK IN PROGRESS AND IS NOT EXPECTED TO WORK OR BE FINAL--
 
 public class CtrlDades {
-    private ProblemManager PM;
-    private PlayerManager PlM;
-    private StatisticsManager SM;
+    private final ProblemManager PM;
+    private final PlayerManager PlM;
+    private final StatisticsManager SM;
     
     public CtrlDades() throws IOException{
         PM = new ProblemManager();
@@ -58,15 +48,53 @@ public class CtrlDades {
     public void storePlayer(String info) throws chessException{
         int cont = 0, word_counter = 0;
         String infoArray[] = info.split(" ");
-        if(infoArray.length > 7) throw new chessException("Too much info");
+        if(infoArray.length > 6) throw new chessException("Too much info");
         for(int i = 0; i < infoArray.length; ++i){
            switch(i){
                 case 0:
-                   if(infoArray[i].length() < 4) throw new chessException("Username mush has more than 3 character");
+                   if(infoArray[i].length() <= 3) throw new chessException("Username mush has more than 3 character");
                    break;
                 case 1:
-                    if(Integer.ge)
+                    try{
+                        Integer.parseInt(infoArray[i]);
+                    }
+                    catch(Exception e){
+                        throw new chessException("Wins must be a number");
+                    }
+                    break;
+                case 2:
+                    try{
+                        Integer.parseInt(infoArray[i]);
+                    }
+                    catch(Exception e){
+                        throw new chessException("Loses must be a number");
+                    }
+                    break;
+                case 3:
+                    try{
+                        Integer.parseInt(infoArray[i]);
+                    }
+                    catch(Exception e){
+                        throw new chessException("ELO must be a number");
+                    }
+                    break;
+                case 4:
+                    try{
+                        Integer.parseInt(infoArray[i]);
+                    }
+                    catch(Exception e){
+                        throw new chessException("OP_rating must be a number");
+                    }
+                case 5:
+                    if(infoArray[i].length() <= 3) throw new chessException("Username mush has more than 3 character");
+                    break;
+                default:
+                    throw new chessException("Too much info");
            }
         }
+    }
+    public void createPlayer(String id, String password){
+        if()
+        
     }
 }
